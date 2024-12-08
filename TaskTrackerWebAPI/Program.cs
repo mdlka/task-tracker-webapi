a7c78d682e1d5b75e2716443f408636059379923
mdlka <mogilevskiy499@gmail.com>
2024-12-08 15:01:35 +0300

    Initial commit

---
+++ TaskTrackerWebAPI/Program.cs
@@ -0,0 +1,29 @@
+namespace TaskTrackerWebAPI
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddControllers();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
