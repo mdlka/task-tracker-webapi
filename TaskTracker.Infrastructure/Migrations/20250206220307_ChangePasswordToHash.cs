ba8d3217bfbc190f292dc2e66857e88ac5e20ce6
mdlka <mogilevskiy499@gmail.com>
2025-02-07 01:26:08 +0300

    Add migration

---
+++ TaskTracker.Infrastructure/Migrations/20250206220307_ChangePasswordToHash.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TaskTracker.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangePasswordToHash : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "password",
+                table: "users_credentials",
+                newName: "password_hash");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "password_hash",
+                table: "users_credentials",
+                newName: "password");
+        }
+    }
+}
